afe07589a6c3642c56f17506239d9cc24b53f4c9
eastraining <21341281+eastraining@users.noreply.github.com>
2021-12-19 22:10:31 +0800

    Complete part 2.10

---
+++ love-tut-pause-modules/conf.lua
@@ -0,0 +1,5 @@
+-- LÖVE configuration file - to enable windows console log
+
+love.conf = function(t)
+  t.console = true
+end
--- love-tut-pause-modules/entities/triangle.lua
+++ love-tut-pause-modules/entities/triangle.lua
@@ -10,5 +10,6 @@ triangle.body.setMass(triangle.body, 32)
 triangle.shape = love.physics.newPolygonShape(100, 100, 200, 100, 200, 200)
 triangle.fixture = love.physics.newFixture(triangle.body, triangle.shape)
 triangle.fixture:setRestitution(0.75)
+triangle.fixture:setUserData('triangle')
 
 return triangle

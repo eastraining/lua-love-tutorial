a80afb400e2b540ff65988b764d46381d403f1c1
eastraining <21341281+eastraining@users.noreply.github.com>
2021-12-20 12:30:39 +0800

    Abstract boundary boxes

--- love-tut-breakout/entities.lua
+++ love-tut-breakout/entities.lua
@@ -1,18 +1,16 @@
 -- entities.lua
 
-local boundary_top = require('entities/boundary-top')
-local boundary_bottom = require('entities/boundary-bottom')
-local boundary_left = require('entities/boundary-left')
-local boundary_right = require('entities/boundary-right')
+local boundary_x = require('entities/boundary-x')
+local boundary_y = require('entities/boundary-y')
 local ball = require('entities/ball')
 local paddle = require('entities/paddle')
 local brick = require('entities/brick')
 
 return {
-  boundary_bottom(400, 606),
-  boundary_left(-6, 300),
-  boundary_right(806, 300),
-  boundary_top(400, -6),
+  boundary_x(400, 606),
+  boundary_y(-6, 300),
+  boundary_y(806, 300),
+  boundary_x(400, -6),
   paddle(300, 500),
   ball(200, 200),
   brick(100, 100),

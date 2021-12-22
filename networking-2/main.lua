08a119348529399647cd39844604cac09d207d39
eastraining <21341281+eastraining@users.noreply.github.com>
2021-12-22 22:07:08 +0800

    Complete networking-2

--- networking-2/main.lua
+++ networking-2/main.lua
@@ -2,6 +2,8 @@
 
 local menu_service = require('services/menu')
 local net_service = require('services/net')
+local entity_service = require('services/entity')
+local input_service = require('services/input')
 
 love.load = function()
   -- Keep text pixels sharp and intact instead of blurring
@@ -19,13 +21,37 @@ love.draw = function()
   menu_service.draw()
   -- Scale up the size of the text being printed
   local transform = love.math.newTransform(0, 0, 0, 3)
-  if net_service.is_connected() then
-    love.graphics.print('peer connected (see console)', transform)
+  if net_service.is_connected() and net_service.is_server() then
+    love.graphics.setColor({1, 1, 1, 1})
+    love.graphics.print('Server preview. See console for details.', transform)
   end
+
+  for _, entity in pairs(entity_service.entities) do
+    entity_service.draw(entity)
+  end
+end
+
+love.keypressed = function(pressed_key)
+  menu_service.handle_keypress(pressed_key)
+  input_service[pressed_key] = true
+end
+
+love.keyreleased = function(released_key)
+  input_service[released_key] = false
 end
 
 love.update = function()
   menu_service.update()
+  -- Check to see if a player has spawned and update its movement if any direction keys are being pressed
+  if entity_service.player_id then
+    local player = entity_service.entities[entity_service.player_id]
+    local old_x = player.x_pos
+    local old_y = player.y_pos
+    entity_service.move()
+    if player.x_pos ~= old_x or player.y_pos ~= old_y then
+      net_service.send('move|' .. player.id .. '|' .. player.x_pos .. '|' .. player.y_pos)
+    end
+  end
   net_service.update()
 end
 
--- networking-2/services/net.lua
+++ networking-2/services/net.lua
@@ -1,6 +1,7 @@
 -- net.lua
 
 local enet = require('enet')
+local entity_service = require('services/entity')
 
 -- Populate one or the other depending if we start a server or client host
 local client_host
@@ -9,6 +10,73 @@ local server_host
 -- As a server, we want to keep track of all the connected clients
 local peers = {}
 local received_data = false
+
+-- Callbacks to invoke when certain events are received from a peer
+-- Define a callback to handle every type of message in our application protocol
+local message_handlers = {
+  ['your-id'] = function(message, event, is_server)
+    local player_id = message[2]
+    local x_pos = message[3]
+    local y_pos = message[4]
+    entity_service.player_id = player_id
+    entity_service.entities[player_id] = entity_service.spawn(player_id, x_pos, y_pos)
+  end,
+  ['peer-id'] = function(message, event, is_server)
+    local player_id = message[2]
+    local x_pos = message[3]
+    local y_pos = message[4]
+    entity_service.entities[player_id] = entity_service.spawn(player_id, x_pos, y_pos)
+  end,
+  ['move'] = function(message, event, is_server)
+    local player_id = message[2]
+    local x_pos = message[3]
+    local y_pos = message[4]
+    entity_service.entities[player_id].x_pos = x_pos
+    entity_service.entities[player_id].y_pos = y_pos
+    if is_server then
+      -- Relay this message to the other players
+      for id, peer in pairs(peers) do
+        if id ~= player_id then
+          peer:send(event.data)
+        end
+      end
+    end
+  end
+}
+-- These event types are defined by Lua-enet. A "receive" type of event
+-- is a generic event that carries any of the messages above.
+local event_handlers = {
+  connect = function(event, is_server)
+    -- Only the server needs to do stuff here on connect
+    if is_server then
+      -- event.peer:connect_id() provides us with a unique number.
+      -- We'll convert that number to a string and use it as the player ID.
+      local player = entity_service.spawn(tostring(event.peer:connect_id()), 100, 100)
+      -- Store this player in the player table with the player ID as the key.
+      entity_service.entities[player.id] = player
+      -- Send the initial "your-id" message back to the connecting client so they can spawn this entity too.
+      event.peer:send('your-id|' .. player.id .. '|' .. player.x_pos .. '|' .. player.y_pos)
+       -- Let all the other peers know about this player
+      for _, peer in pairs(peers) do
+        local peer_player = entity_service.entities[tostring(peer:connect_id())]
+        peer:send('peer-id|' .. player.id .. '|' .. player.x_pos .. '|' .. player.y_pos)
+        event.peer:send('peer-id|' .. peer_player.id .. '|' .. peer_player.x_pos .. '|' .. peer_player.y_pos)
+      end
+      -- Add this peer to the peer list
+      peers[tostring(event.peer:connect_id())] = event.peer
+    end
+  end,
+  disconnect = function(event, is_server)
+  end,
+  receive = function(event, is_server)
+    -- Extract the message out from the event and call the appropriate callback above
+    local message = {}
+    for match in (event.data .. '|'):gmatch('(.-)|') do
+      table.insert(message, match)
+    end
+    message_handlers[message[1]](message, event, is_server)
+  end
+}
 
 -- The service we will be returning
 local net = {}
@@ -40,16 +108,21 @@ net.update = function()
   local event = host:service()
   if event then
     received_data = true
+    -- event.type will be either "connect", "disconnect", or "receive"
+    event_handlers[event.type](event, net.is_server())
+    -- Print out the event table for debug purposes
     print('----')
     for k, v in pairs(event) do
       print(k, v)
     end
-    if net.is_client() then
-      event.peer:send('meow')
-    else
-      print(event.peer:connect_id())
-      event.peer:send('bark')
-    end
+  end
+end
+
+net.send = function(message)
+  if net.is_client() then
+    server_host:send(message)
+  else
+    server_host:broadcast(message)
   end
 end
 
